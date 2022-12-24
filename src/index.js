095ac695661fe23dc6fe9086fd71c61ad5ac84a0
KishiKyosuke（Kyo18） <carp011808pg@icloud.com>
2022-12-24 03:35:27 +0000

    JSを追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value
+  document.getElementById('add-text').value = ''
+  const div = document.createElement('div')
+  console.log(div)
+  alert(inputText)
+}
+
+document.getElementById("add-button").addEventListener('click', () => onClickAdd())
